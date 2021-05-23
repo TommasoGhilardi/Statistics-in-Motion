abfcf638e90c4bd0729c0e08265c557a95830b7d
Tommaso Ghilardi <38372956+TommasoGhilardi@users.noreply.github.com>
2021-05-23 12:56:46 +0200

    Add files via upload

---
+++ RejectVisualCoding.m
@@ -0,0 +1,29 @@
+function [Configuration] = RejectVisualCoding(Configuration)
+
+% Read the CSV
+colnames  = {'BeginTime_msec','EndTime_msec','Duration_msec','Goodness',...
+    'Annotations','ActionExecution'};
+T = readtable([InPath Subject '\ActionExecution.csv']);
+T.Properties.VariableNames = colnames;
+
+% Check if there is the same number of trials between videocoding and EEG data
+Trials = T(ismember( T{:,'Goodness'}, {'Accept', 'Reject'}),:);
+
+if length(Trials{:,'ActionExecution'}) ~= length(Configuration.trl)
+    % Error if the number is different
+    error(['Error!!!!!!!!!' newline ... 
+        'Trials defined during videocoding mismatch trials in the EEG data.' newline ...
+        'Please check the videocoding or the trialdefinition'])
+
+else
+    
+    % If it the same number elimintaes the bad ones
+    Bad_ones = ismember( Trials{:,'Goodness'}, 'Reject');
+    Configuration.trl(Bad_ones ,:)= [];    
+    
+end
+
+
+end
+
+
