12dc6484d8d4a104625daf31f8d697d2e4c97952
Tommaso Ghilardi <38372956+TommasoGhilardi@users.noreply.github.com>
2021-05-23 12:54:20 +0200

    Update
    added rereference
    added outputpath
    cleaned code

--- EEG_analysis.m
+++ EEG_analysis.m
@@ -6,8 +6,18 @@ addpath('C:\Users\krav\Documents\Matlab\fieldtrip');    % add fieltrip as your t
 ft_defaults();      % set all the default fieltrip functions
 
 % Data Subject settings
-Path = 'C:\Users\krav\Desktop\BabyBrain\Internship\Monique\Analysis\';       %location of the participant data
+InPath  = 'C:\Users\krav\Desktop\BabyBrain\Internship\Monique\Data\Raw\';       %location of the participant data
+OutPath = 'C:\Users\krav\Desktop\BabyBrain\Internship\Monique\Data\Out\';
 Subject = 'p1';
+
+% Create output folder if it dosen't exist
+if ~exist([OutPath Subject], 'dir')
+   mkdir([OutPath Subject])
+   mkdir([OutPath Subject '\Prediction'])
+   mkdir([OutPath Subject '\Execution'])
+end
+
+SavingLocation = [OutPath Subject '\Execution\'];
 
 % Triggers specification
 Triggers.fixation_cross = 'S 50';
@@ -17,12 +27,12 @@ Triggers.predictive_window.high          = {'S 13','S 23','S 33','S 43'};
 Triggers.predictive_window.deterministic = {'S 14','S 24','S 34','S 44'};
 
 % Channels of interest definition
-Channels.motor = {'C3','Cz','C4'};
+Channels.motor     = {'C3','Cz','C4'};
 Channels.occipital = {'O1','Oz','O2'};
 
 % Frequencies
-Frequencies.value   = [9 11; 17 20];
-Frequencies.names = ['alpha'; 'beta'];
+Frequencies.value = [9 11; 17 20];
+Frequencies.names = ["alpha"; "beta"];
 
 % Cap configuration used to plot with layout
 cap_conf = 'acticap-64ch-standard2.mat';
@@ -32,22 +42,21 @@ cap_conf = 'acticap-64ch-standard2.mat';
 
 %%%%% Segmenting definition %%%%%
 cfg                         = [];
-cfg.dataset                 = [Path,Subject,'.eeg'];
+cfg.dataset                 = [InPath,Subject,'\' Subject '.eeg'];
 cfg.trialdef.eventtype      = 'Stimulus';
 cfg.trialdef.eventvalue     = [Triggers.predictive_window.low,...
     Triggers.predictive_window.medium, Triggers.predictive_window.high...
     Triggers.predictive_window.deterministic, Triggers.fixation_cross]; % the value of the stimulus trigger for fully incongruent (FIC).
-cfg.trialdef.prestim        = 0.25; % in seconds
-cfg.trialdef.poststim       = 1.25; % in seconds
+cfg.trialdef.prestim        = 0; % in seconds
+cfg.trialdef.poststim       = 1; % in seconds
 cfg = ft_definetrial(cfg);
 
-%%%%% read data and segment %%%%%
+%%%%% Read data and segment %%%%%
 cfg.hpfilter    = 'yes';        % enable high-pass filtering
 cfg.lpfilter    = 'yes';        % enable low-pass filtering
 cfg.hpfreq      = 1;            % set up the frequency for high-pass filter
 cfg.lpfreq      = 40;
 cfg.detrend     = 'yes';
-cfg.demean      = 'yes';    
 data = ft_preprocessing(cfg); % read raw data
 
 if isequal(data.label{end},'FP1')
@@ -57,7 +66,8 @@ end
 %%%% Plot %%%%
 cfg = [];
 cfg.viewmode = 'vertical';
-ft_databrowser(cfg, data);
+a  =ft_databrowser(cfg, data);
+save([SavingLocation 'Epoched.mat'],'data');
 
 
 %% ========================% Artifacts %======================= %%
@@ -66,7 +76,7 @@ ft_databrowser(cfg, data);
 cfg = [];
 cfg.metric      = 'kurtosis';  % use by default kurtosis method
 cfg.method      = 'summary'; % use by default summary method
-cfg.latency     = [0 1];
+cfg.keepchannel = 'nan';
 art1_data       = ft_rejectvisual(cfg,data);
 
 
@@ -93,17 +103,26 @@ reject      = str2num(char(answer{1}));
 
 close all
 
+cfg = [];
+cfg.layout = cap_conf;
+cfg.component   = 1:2; % specify the layout file that should be used for plotting
+cfg.viewmode = 'component';
+ft_databrowser(cfg, components)
+
+
 % Control the components
-% cfg = [];
-% cfg.rejcomp  = reject;
-% cfg.powscale = 'linear';
-% cfg.layout   = cap_conf; % specify the layout file that should be used for plotting
-% reject = ft_icabrowser(cfg, components);
+cfg = [];
+cfg.rejcomp  = reject;
+cfg.powscale = 'linear';
+cfg.layout   = cap_conf; % specify the layout file that should be used for plotting
+reject = ft_icabrowser(cfg, components);
 
 % Rejecting
 cfg             = [];
-cfg.component   = answer; % to be removed component(s)
+cfg.demean      = 'no';
+cfg.component   = reject; % to be removed component(s)
 ica_data = ft_rejectcomponent(cfg, components, art1_data);
+save([SavingLocation 'ICA.mat'],'ica_data');
 
 
 %%%%% Final visual rejection %%%%%
@@ -119,16 +138,18 @@ art2_data = ft_rejectvisual(cfg,ica_data);
 cfg = [];
 cfg.method  = 'trial';
 cfg.keepchannel = 'nan';
-cfg.latency     = [0 1];
 art_final_data = ft_rejectvisual(cfg,art2_data);
+save([SavingLocation 'Clean.mat'],'art_final_data');
 
 
 %% ========================% Frequencies Extraction %======================= %%
 
-% Cut the data
+% Rereference to average of all channels
 cfg = [];
-cfg.toilim = [0 1];
-art_final_data = ft_redefinetrial(cfg, art_final_data);
+cfg.reref      = 'yes';
+cfg.refmethod  = 'avg';
+cfg.refchannel = 'all';
+final_data = ft_preprocessing(cfg, data_orig);
 
 % FFT decomposition
 cfg              = [];
@@ -140,10 +161,10 @@ cfg.pad          = 2;
 cfg.padtype      = 'mean';
 cfg.keeptrials   = 'yes';
 cfg.foi          = 2:1:30; % analysis 2 to 30 Hz in steps of 1 Hz
-Freq_data        = ft_freqanalysis(cfg, art_final_data);
+Freq_data        = ft_freqanalysis(cfg, final_data);
 
 Freq_data.powspctrm = log10(Freq_data.powspctrm); %normalizing data using log10
-
+save([SavingLocation 'FFT.mat'],'Freq_data');
 
 %% ========================% CSV export %======================= %%
 
@@ -182,7 +203,7 @@ Col_trialinfo = Col_trialinfo(:,2);
 CV = table(Col_subject, Col_frequency, Col_channels, Col_trialinfo, Col_power );
 CV.Properties.VariableNames = {'Id','Frequency','Channels','Trial','Power'};
 
-writetable(T,[Path Subject '_data.csv'])
+writetable(T,[SavingLocation 'DF.csv']);
 
 
 
